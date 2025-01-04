36afa90ed26ad62a88e0ba1b1d3ce7f3b8e8654f
study-kim7507 <study.kim7507@gmail.com>
2025-01-04 16:33:47 +0900

    [Silver V] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,17 @@
+// 9656. 돌 게임 2
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	if (n % 2 == 0) cout << "SK" << "\n";
+	else cout << "CY" << "\n";
+
+	return 0;
+}
