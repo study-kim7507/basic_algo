7f72c37b8634b46c6c364b917ff50711971424f1
study-kim7507 <study.kim7507@gmail.com>
2025-05-12 15:08:51 +0900

    [level 1] Title: 폰켓몬, Time: 1.08 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -2,18 +2,14 @@
 #include <map>
 using namespace std;
 
-map<int, int> m;
-
 int solution(vector<int> nums)
 {
-    int cnt = 0;
-    for (int i = 0; i < nums.size(); i++)
-        m[nums[i]]++;
-    for (int i = 1; i <= 200000; i++)
-    {
-        if (m[i] != 0) cnt++;
-        if (cnt >= nums.size() / 2) return cnt; 
-    }
-        
-    return cnt;
+    map<int, int> m;
+    for (auto& e : nums)
+        m[e]++;
+    
+    int size = nums.size() / 2;
+    
+    if (m.size() >= size) return size;
+    else return m.size();
 }
