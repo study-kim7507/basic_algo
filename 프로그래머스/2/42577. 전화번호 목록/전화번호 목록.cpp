0c0c528463a1f6f46a770e9422a44fb9532e28ac
study-kim7507 <study.kim7507@gmail.com>
2025-05-12 15:35:13 +0900

    [level 2] Title: 전화번호 목록, Time: 763.51 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,22 +1,23 @@
 #include <string>
 #include <vector>
 #include <map>
-
 using namespace std;
-map<string, int> m;
+
 
 bool solution(vector<string> phone_book) {
-    for (int i = 0; i < phone_book.size(); i++)
-        m[phone_book[i]]++;
+    map<string, int> m;
+    for (auto& e : phone_book)
+        m[e]++;
     
-    for (int i = 0; i < phone_book.size(); i++)
+    for (auto& e : phone_book)
     {
-        string str;
-        for (int j = 0; j < phone_book[i].size(); j++)
+        string temp = "";
+        for (int i = 0; i < e.size() - 1; i++)
         {
-            str += phone_book[i][j];
-            if (m[str] != 0 && str != phone_book[i]) return false;
+            temp += e[i];
+            if (m[temp] != 0) return false;
         }
     }
+    
     return true;
 }
