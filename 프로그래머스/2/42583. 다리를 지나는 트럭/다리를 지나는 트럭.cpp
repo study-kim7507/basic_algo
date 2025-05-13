ffad3fbefbd23e2c02b573d3450406e9f891e355
study-kim7507 <study.kim7507@gmail.com>
2025-05-13 15:41:50 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.41 ms, Memory: 3.7 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -1,49 +1,42 @@
-#include <string>
 #include <iostream>
+#include<algorithm>
+#include <functional>         // greater 사용 위해 필요  
 #include <vector>
-#include <queue>
+#include<queue>
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    queue<int> remainTrucks;    // 남은 대기 트럭
-    queue<pair<int, int>> onBridgeTrucks;  // {현재 다리 위에 있는 트럭, 다리 위에 올라온 시간}
-    
-    int currentWeight = 0;      // 현재 다리 위에 있는 트럭의 총 무게
-    int time = 0;               // 경과 시간
-    
-    
-    for (auto& e : truck_weights)
-        remainTrucks.push(e);
-    
-    while(!remainTrucks.empty() || !onBridgeTrucks.empty())
+    int answer = 0;
+    int count = 0;
+    int Time = 0; 
+    int Truck_weight = 0;
+    queue<pair<int, int>> truck_move;
+
+    while (true)
     {
-        time++;
-        
-        // 다리 위에 트럭이 있다면 현재 시간과 트럭이 다리에 올라온 시간을 비교
-        // 다 건넜다고 판단되면, 다리에서 제거
-        if (!onBridgeTrucks.empty())
+        if (weight >= Truck_weight + truck_weights.at(count))
         {
-            int enterTime = onBridgeTrucks.front().second;
-            if (time - enterTime >= bridge_length)
+            truck_move.push(make_pair(truck_weights.at(count), bridge_length + 1 + Time));
+            Truck_weight += truck_weights.at(count);
+            count++;
+        }
+
+        if (count >= truck_weights.size())
+        {
+            answer = truck_move.back().second;
+            break;
+        }
+        else
+        {
+            Time++;
+            if (truck_move.front().second == Time+1)
             {
-                currentWeight -= onBridgeTrucks.front().first;
-                onBridgeTrucks.pop();
+                Truck_weight -= truck_move.front().first;
+                truck_move.pop();
             }
         }
-        
-        // 남은 대기 트럭 있다면, 다리 위의 트럭 올라갈 수 있는지 판단하여 올림
-        if (!remainTrucks.empty())
-        {
-            // 개수 및 무게 판단
-            if (onBridgeTrucks.size() < bridge_length && 
-               currentWeight + remainTrucks.front() <= weight)
-            {
-                currentWeight += remainTrucks.front();
-                onBridgeTrucks.push({remainTrucks.front(), time});
-                remainTrucks.pop();
-            }
-        }
+
     }
-    
-    return time;
+
+    return answer;
 }
