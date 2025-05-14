ae71e50fd445d004247e6d5c03a40cdbd305ea4e
study-kim7507 <study.kim7507@gmail.com>
2025-05-14 17:44:40 +0900

    [level 3] Title: 네트워크, Time: 0.10 ms, Memory: 4.43 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,42 +1,44 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <iostream>
-
 using namespace std;
 
+bool vis[201];
 vector<int> adj[201];
 
-int bfs(int n, vector<vector<int>> computers)
-{
-    int ans = 0;
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            if (i != j && computers[i][j] == 1) adj[i].push_back(j);
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
     
-    bool vis[201];
-    for (int i = 0; i < n; i++) vis[i] = false;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (computers[i][j] == 1)
+                adj[i].push_back(j);
+        }
+    }
     
     for (int i = 0; i < n; i++)
     {
         if (vis[i] == true) continue;
+        
         queue<int> q;
-        q.push(i); vis[i] = true;
+        q.push(i);
+        vis[i] = true;
+        
         while (!q.empty())
         {
             auto cur = q.front(); q.pop();
             for (auto nxt : adj[cur])
             {
-                if (vis[nxt] == true) continue;
-                q.push(nxt); vis[nxt] = true;
+                if (vis[nxt]) continue;
+                vis[nxt] = true;
+                q.push(nxt);
             }
         }
-        ans++;
+        
+        answer++;
     }
     
-    return ans;
-}
-
-int solution(int n, vector<vector<int>> computers) {
-    return bfs(n, computers);
+    return answer;
 }
