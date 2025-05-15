96aeca29e68497357ce1c98d1fe8aeb2c500fffd
study-kim7507 <study.kim7507@gmail.com>
2025-05-15 14:30:29 +0900

    [level 3] Title: 이중우선순위큐, Time: 9.85 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -1,16 +1,11 @@
 #include <string>
-#include <queue>
 #include <vector>
-#include <unordered_map>
-#include <queue>
-#include <algorithm>
+#include <set>
 
 using namespace std;
 
-vector<int> solution(vector<string> operations) {    
-    unordered_map<int, int> m;
-    priority_queue<int, vector<int>, greater<int>> minHeap;
-    priority_queue<int> maxHeap;
+vector<int> solution(vector<string> operations) {
+    multiset<int> ms;
     
     for (int i = 0; i < operations.size(); i++)
     {
@@ -20,58 +15,20 @@ vector<int> solution(vector<string> operations) {
             s_num += operations[i][j];
         int num = stoi(s_num);
         
-        // 삽입
-        if (cmd == 'I')
+        if (cmd == 'I') ms.insert(num);
+        else if (cmd == 'D' && !ms.empty())
         {
-            m[num]++;
-            maxHeap.push(num);
-            minHeap.push(num);
-        }
-        // 삭제
-        else if (cmd == 'D')
-        {
-            // 최댓값 삭제 (maxHeap)
-            if (num == 1)
-            {
-                while (!maxHeap.empty())
-                {
-                    int top = maxHeap.top(); maxHeap.pop();
-                    if (m[top] != 0)
-                    {
-                        m[top]--;
-                        break;
-                    }
-                }
-                    
-            }
-            // 최솟값 삭제 (minHeap)
-            else if (num == -1)
-            {
-                while (!minHeap.empty())
-                {
-                    int top = minHeap.top(); minHeap.pop();
-                    if (m[top] != 0)
-                    {
-                        m[top]--;
-                        break;
-                    }
-                }
-            }
+            if (num == 1) ms.erase(--ms.end());
+            else if (num == -1) ms.erase(ms.begin());
         }
     }
     
-    while(!maxHeap.empty() && m[maxHeap.top()] == 0)
-        maxHeap.pop();
-    
-    while(!minHeap.empty() && m[minHeap.top()] == 0)
-        minHeap.pop();
-    
-    vector<int> ans;
-    if (maxHeap.empty()) return vector<int>(2, 0);
+    if (ms.empty()) return vector<int>(2, 0);
     else 
     {
-        ans.push_back(maxHeap.top());
-        ans.push_back(minHeap.top());
+        vector<int> ans;
+        ans.push_back(*(--ms.end()));
+        ans.push_back(*(ms.begin()));
+        return ans;
     }
-    return ans;
 }
