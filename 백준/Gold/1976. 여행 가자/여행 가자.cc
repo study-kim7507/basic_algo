1e0a85074ad3cfae2a1d5ee565a24f6537d1c382
study-kim7507 <study.kim7507@gmail.com>
2025-05-18 15:58:03 +0900

    [Gold IV] Title: 여행 가자, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.cc
+++ 백준/Gold/1976. 여행 가자/여행 가자.cc
@@ -10,6 +10,23 @@ int n, m;
 
 vector<int> edges[201];
 vector<int> plan;
+int parent[201];
+bool vis[201];
+
+int find(int a)
+{
+	if (a != parent[a]) return find(parent[a]);
+	return a;
+}
+
+void uni(int a, int b)
+{
+	a = find(a);
+	b = find(b);
+
+	if (a < b) parent[b] = a;
+	else parent[a] = b;
+}
 
 int main()
 {
@@ -17,6 +34,10 @@ int main()
 	cin.tie(0); cout.tie(0);
 
 	cin >> n >> m;
+
+	for (int i = 0; i < n; i++)
+		parent[i] = i;
+
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
@@ -37,40 +58,36 @@ int main()
 		plan.push_back(num - 1);
 	}
 
-	for (int i = 0; i < m - 1; i++)
+	int cnt = 0;
+
+	int st = plan[0];
+	queue<int> q;
+	q.push(st);
+	
+	while (!q.empty())
 	{
-		int st = plan[i];
-		int dest = plan[i + 1];
-		vector<bool> vis(n, false);
-		
-		queue<int> q;
-		q.push(st);
-		vis[st] = true;
-
-		bool can = false;
-		while (!q.empty())
+		auto cur = q.front(); q.pop();
+		for (auto nxt : edges[cur])
 		{
-			auto cur = q.front(); q.pop();
-			if (cur == dest)
-			{
-				can = true;
-				break;
-			}
-			for (auto nxt : edges[cur])
-			{
-				if (vis[nxt]) continue;
-				vis[nxt] = true;
-				q.push(nxt);
-			}
-		}
+			if (find(cur) == find(nxt)) continue;
 
-		if (!can)
-		{
-			std::cout << "NO" << "\n";
-			return 0;
+			uni(cur, nxt);
+			cnt++;
+
+			if (cnt == n - 1) break;
+
+			q.push(nxt);
 		}
 	}
 
-	std::cout << "YES" << "\n";
+	bool can = true;
+	for (int i = 1; i < plan.size(); i++)
+	{
+		if (find(plan[i - 1]) != find(plan[i])) can = false;
+	}
+
+	if (can) std::cout << "YES";
+	else std::cout << "NO";
+
 	return 0;
 }
