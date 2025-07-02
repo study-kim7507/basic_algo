2826fc8a4a92285fb0455ddd5b23ebdd63500bb2
study-kim7507 <study.kim7507@gmail.com>
2025-07-02 15:29:27 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 60 ms, Memory: 15560 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -1,88 +1,79 @@
-// 2206. 벽 부수고 이동하기
-
+// BOJ_2206. 벽 부수고 이동하기
 #include <iostream>
 #include <queue>
-#include <tuple>
 #include <algorithm>
 using namespace std;
+
+#define X first
+#define Y second
 
 const int dx[4] = { -1, 0, 1, 0 };
 const int dy[4] = { 0, -1, 0, 1 };
 
+int n, m;
 int board[1001][1001];
-int vis[1001][1001][2];
+int dist[1001][1001][2];	// 0 : 벽을 안부수고 이동, 1 : 벽을 부수고 이동
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
-	int n, m;
 	cin >> n >> m;
 	for (int i = 0; i < n; i++)
 	{
-		string input;
-		cin >> input;
+		string row;
+		cin >> row;
 		for (int j = 0; j < m; j++)
-		{
-			board[i][j] = static_cast<int>(input[j] - '0');
-		}
+			board[i][j] = (int)(row[j] - '0');
 	}
 
-	queue<tuple<int, int, int>> q;
-	vis[0][0][0] = 1;
-	q.push({ 0, 0, 0 });
+	queue<pair<bool, pair<int, int>>> q;
+	q.push({ false, {0, 0} });
+	dist[0][0][0] = 1;
 
 	while (!q.empty())
 	{
-		auto cur = q.front(); q.pop();
-		bool isBroken = static_cast<bool>(get<2>(cur));
+		bool isBroken = q.front().first;
+		auto curPos = q.front().second;
+		q.pop();
 		for (int dir = 0; dir < 4; dir++)
 		{
-			int nx = get<0>(cur) + dx[dir];
-			int ny = get<1>(cur) + dy[dir];
+			int nx = curPos.X + dx[dir];
+			int ny = curPos.Y + dy[dir];
 
-			if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-			if (vis[nx][ny][get<2>(cur)] != 0) continue;
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;	// 범위 밖
+			if (isBroken && dist[nx][ny][1]) continue;
+			if (!isBroken && dist[nx][ny][0]) continue;
 
-			// 벽을 부수고 온 경우
-			if (isBroken)
+			if (board[nx][ny] == 0)
 			{
-				// 이미 벽을 부수고 왔기 때문에 추가적인 벽을 부술 수 없음
-				if (board[nx][ny] == 1) continue;
-			
-				vis[nx][ny][1] = vis[get<0>(cur)][get<1>(cur)][1] + 1;
-				q.push({ nx, ny, 1 });
-			}
-
-			// 벽을 부수지 않고 온 경우
-			else
-			{
-				// 벽을 부수지 않고 왔는데, 벽을 만난 경우
-				if (board[nx][ny] == 1)
+				if (isBroken)
 				{
-					// 현재 만난 벽을 부술 경우
-					vis[nx][ny][1] = vis[get<0>(cur)][get<1>(cur)][0] + 1;
-					q.push({ nx, ny, 1 });
+					dist[nx][ny][1] = dist[curPos.X][curPos.Y][1] + 1;
+					q.push({ true, {nx, ny} });
 				}
 				else
 				{
-
-					vis[nx][ny][0] = vis[get<0>(cur)][get<1>(cur)][0] + 1;
-					q.push({ nx, ny, 0 });
+					dist[nx][ny][0] = dist[curPos.X][curPos.Y][0] + 1;
+					q.push({ false, {nx, ny} });
 				}
-
+			}
+			else if (board[nx][ny] == 1)
+			{
+				if (isBroken) continue;	// 이미 부수고 왔기에, 더 이상 부술 수 없음
+				else dist[nx][ny][1] = dist[curPos.X][curPos.Y][0] + 1;
+				q.push({ true, {nx, ny} });
 			}
 		}
 	}
 
-	int result = 987654321;
+	int unbrokenDist = dist[n - 1][m - 1][0];
+	int brokenDist = dist[n - 1][m - 1][1];
 
-	if (vis[n - 1][m - 1][0] != 0) result = min(result, vis[n - 1][m - 1][0]);
-	if (vis[n - 1][m - 1][1] != 0) result = min(result, vis[n - 1][m - 1][1]);
-	
-	if (result == 987654321) cout << -1 << "\n";
-	else cout << result << "\n";
-
+	if (unbrokenDist == 0 && brokenDist == 0) std::cout << -1 << "\n";
+	else if (unbrokenDist == 0) std::cout << brokenDist << "\n";
+	else if (brokenDist == 0) std::cout << unbrokenDist << "\n";
+	else std::cout << min(unbrokenDist, brokenDist) << "\n";
 	return 0;
 }
