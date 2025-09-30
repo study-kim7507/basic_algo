dc05753ff44ad501d2d31952f69636c1788cd4da
study-kim7507 <study.kim7507@gmail.com>
2025-09-30 16:48:32 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2044 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,10 +1,10 @@
-// BOJ_2579. 계단 오르기
+// BOJ_1149 RGB거리
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
 int n;
-int arr[1001][3];
+int cost[1001][3];
 int dp[1001][3];
 
 int main()
@@ -15,20 +15,19 @@ int main()
 	cin >> n;
 	for (int i = 1; i <= n; i++)
 		for (int j = 0; j < 3; j++)
-			cin >> arr[i][j];
+			cin >> cost[i][j];
 
-	dp[1][0] = arr[1][0];
-	dp[1][1] = arr[1][1];
-	dp[1][2] = arr[1][2];
+	dp[1][0] = cost[1][0];
+	dp[1][1] = cost[1][1];
+	dp[1][2] = cost[1][2];
 
 	for (int i = 2; i <= n; i++)
 	{
-		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
-		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
-		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + arr[i][2];
+		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
+		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
+		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
 	}
-
-	std::cout << min({ dp[n][0], dp[n][1], dp[n][2] }) << "\n";
-
+	
+	cout << min({ dp[n][0], dp[n][1], dp[n][2] }) << "\n";
 	return 0;
 }
