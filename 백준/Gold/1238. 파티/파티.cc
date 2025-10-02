ecca132b4b9bb4a802f3d46a59466dd8014b68a3
study-kim7507 <study.kim7507@gmail.com>
2025-10-02 15:01:46 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2292 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,71 +1,59 @@
-// 다익스트라 풀이
-// 시작 정점에서 모든 정점까지의 최단거리
-// N번의 다익스트라, 시작 마을에서 파티가 열리는 마을까지의 거리 + 파티가 열리는 마을에서 시작 마을까지의 거리가 최대가 되는 경우를 찾으면 된다
-
+// BOJ 1238. 파티
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <limits>
+#include <queue>
+#include <climits>
 #include <algorithm>
 using namespace std;
 
 int n, m, x;
+int cost[1001];
 
-// {거리, 정점}
-vector<pair<int, int>> adj[1001];
-const int INF = 0x3f3f3f3f;
-int d[1001];
-
-int dijkstra(int st)
+void dijkstra(vector<pair<int, int>> (&e)[1001])
 {
-	fill(d, d + n + 1, INF);
+	int dist[1001];
+	fill(dist, dist + 1001, INT_MAX);
+
 	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+	dist[x] = 0;
+	pq.push({ dist[x], x });
 	
-	d[st] = 0;
-	pq.push({ d[st], st }); // {거리, 정점}
 	while (!pq.empty())
 	{
 		auto cur = pq.top(); pq.pop();
-		if (d[cur.second] != cur.first) continue;
-		for (auto nxt : adj[cur.second])
+		if (dist[cur.second] != cur.first) continue;	
+		for (auto nxt : e[cur.second])
 		{
-			if (d[nxt.second] <= d[cur.second] + nxt.first) continue;
-			d[nxt.second] = d[cur.second] + nxt.first;
-			pq.push({ d[nxt.second], nxt.second });
+			if (dist[nxt.first] <= cur.first + nxt.second) continue;
+			dist[nxt.first] = cur.first + nxt.second;
+			pq.push({ dist[nxt.first], nxt.first });
 		}
 	}
 
-	return d[x];
+	for (int i = 1; i <= n; i++)
+		cost[i] += dist[i];
 }
 
 int main()
 {
 	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+	cin.tie(0); cout.tie(0);
+
+	vector<pair<int, int>> edges[1001];
+	vector<pair<int, int>> revEdges[1001];
 
 	cin >> n >> m >> x;
 	for (int i = 0; i < m; i++)
 	{
-		int u, v, t;
-		cin >> u >> v >> t;
-		adj[u].push_back({ t, v });
+		int u, v, c;
+		cin >> u >> v >> c;
+		edges[u].push_back({ v, c });
+		revEdges[v].push_back({ u, c });
 	}
 
-	// 각 마을에서 파티가 열리는 마을까지 가는데 드는 거리
-	int result[1001];
-	for (int i = 1; i <= n; i++)
-		result[i] = dijkstra(i);
-	
-	// 파티가 열리는 마을에서 각 마을로 되돌아가는 거리를 합산하며 최대값을 찾음
-	dijkstra(x);
-	int m = numeric_limits<int>::min();
-	for (int i = 1; i <= n; i++)
-	{
-		result[i] += d[i];
-		m = max(m, result[i]);
-	}
-		
-	cout << m;
+	dijkstra(edges);	// x 마을에서 각 마을까지의 최단거리 계산
+	dijkstra(revEdges); // 각 마을에서 x 마을까지의 최단거리 계산
+
+	cout << *max_element(cost + 1, cost + n + 1) << "\n";
 	return 0;
 }
