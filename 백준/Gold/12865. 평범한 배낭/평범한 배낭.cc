c915344947b46777194c8c1b5b3b31c3f300629c
study-kim7507 <study.kim7507@gmail.com>
2025-10-05 13:44:23 +0900

    [Gold V] Title: 평범한 배낭, Time: 4 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,39 +1,37 @@
-// BOJ_12865. 평범한 배낭
+// BOJ 12865. 평범한 배낭
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
-
-int n, k;
-
-// {무게, 가치}
-int weights[101];
-int values[101];
-// dp[i][w] : 최대 무게가 w인 가방에서 i번째 물건까지 판단했을 때의 최대 가치
-int dp[101][100001];
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
+	int n, k;
 	cin >> n >> k;
+
+	// {무게, 가치}
+	vector<pair<int, int>> things(n + 1);
 	for (int i = 1; i <= n; i++)
 	{
-		cin >> weights[i];
-		cin >> values[i];
+		int w, v;
+		cin >> w >> v;
+		things[i] = { w, v };
 	}
 
-	for (int i = 1; i <= n; i++)
+	vector<int> dp(k + 1, 0);
+	for (int i = 0; i <= k; i++)
 	{
-		for (int j = 1; j <= k; j++)
-		{
-			if (weights[i] > j) dp[i][j] = dp[i - 1][j];
-			else if (weights[i] <= j) dp[i][j] = max(dp[i - 1][j], values[i] + dp[i - 1][j - weights[i]]);
-		}
+		if (things[1].first <= i)
+			dp[i] = things[1].second;
 	}
 
-	std::cout << dp[n][k];
+	for (int i = 2; i <= n; i++)
+		for (int j = k; j >= things[i].first; j--)
+			dp[j] = max(dp[j], dp[j - things[i].first] + things[i].second);
+
+	std::cout << dp[k] << "\n";
 	return 0;
 }
