a2115c52a74386652958b31742101c03e42c3e76
study-kim7507 <study.kim7507@gmail.com>
2025-10-24 17:11:00 +0900

    [Gold IV] Title: 플로이드, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,52 +1,59 @@
+// BOJ 11404. 플로이드
 #include <iostream>
+#include <climits>
+#include <algorithm>
 using namespace std;
 
-#define INF 10000000
-
-int dist[101][101];
+#define INF 987654321
 
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
 
 	int n, m;
 	cin >> n >> m;
 
-	fill(dist[0], dist[101], INF);
+	// 초기화
+	int board[101][101];
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if (i == j) board[i][j] = 0;
+			else board[i][j] = INF;
+		}
+	}
+		
 
-	for (int i = 0; i <= n; i++)
-		dist[i][i] = 0;
-
+	// 입력
 	for (int i = 0; i < m; i++)
 	{
 		int a, b, c;
 		cin >> a >> b >> c;
-		dist[a][b] = min(dist[a][b], c);
+
+		board[a][b] = min(board[a][b], c);
 	}
-
-	
-
-	for (int k = 1; k <= n; k++)
-	{
-		for (int i = 1; i <= n; i++)
-		{
-			for (int j = 1; j <= n; j++)
-			{
-				dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
-			}
-		}
-	}https://www.acmicpc.net/problem/11404
 
 	for (int i = 1; i <= n; i++)
 	{
 		for (int j = 1; j <= n; j++)
 		{
-			if (dist[i][j] == INF) cout << 0 << " ";
-			else cout << dist[i][j] << " ";
+			for (int k = 1; k <= n; k++)
+			{
+				board[j][k] = min(board[j][k], board[j][i] + board[i][k]);
+			}
 		}
-		cout << "\n";
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if (board[i][j] == INF) std::cout << 0 << " ";
+			else std::cout << board[i][j] << " ";
+		}
+		std::cout << "\n";
 	}
 
 	return 0;
