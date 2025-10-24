467030180757aadef3845bf7f1d0a4436a420819
study-kim7507 <study.kim7507@gmail.com>
2025-10-24 18:20:25 +0900

    [Gold II] Title: 플로이드 2, Time: 24 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Gold/11780. 플로이드 2/플로이드 2.cc
+++ 백준/Gold/11780. 플로이드 2/플로이드 2.cc
@@ -1,47 +1,55 @@
-// 플로이드 워셜 경로복원 문제
+// BOJ 11780. 플로이드 2
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int n, m;
-int dist[101][101];
-int nxt[101][101];
+#define INF 987654321
 
-const int INF = 987654321;
+
+int board[101][101];
+int nxt[101][101];
 
 int main()
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0); cout.tie(0);
 
+	int n, m;
 	cin >> n >> m;
+
+	// 초기화
 	for (int i = 1; i <= n; i++)
 	{
-		fill(dist[i] + 1, dist[i] + 1 + n, INF);
-		dist[i][i] = 0;
+		for (int j = 1; j <= n; j++)
+		{
+			if (i == j) continue;
+			else board[i][j] = INF;
+		}
 	}
 
 	for (int i = 0; i < m; i++)
 	{
 		int a, b, c;
 		cin >> a >> b >> c;
-		if (dist[a][b] > c)
+
+		if (c < board[a][b])
 		{
-			dist[a][b] = c;
+			board[a][b] = c;
 			nxt[a][b] = b;
 		}
 	}
-		
-	for (int k = 1; k <= n; k++)
+
+	for (int i = 1; i <= n; i++)
 	{
-		for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
 		{
-			for (int j = 1; j <= n; j++)
+			for (int k = 1; k <= n; k++)
 			{
-				if (dist[i][k] + dist[k][j] < dist[i][j])
+				if (board[j][k] > board[j][i] + board[i][k])
 				{
-					dist[i][j] = dist[i][k] + dist[k][j];
-					nxt[i][j] = nxt[i][k];
+					board[j][k] = board[j][i] + board[i][k];
+					nxt[j][k] = nxt[j][i];
 				}
 			}
 		}
@@ -51,35 +59,37 @@ int main()
 	{
 		for (int j = 1; j <= n; j++)
 		{
-			if (dist[i][j] == INF) cout << 0 << " ";
-			else cout << dist[i][j] << " ";
+			if (board[i][j] == INF) std::cout << 0 << " ";
+			else std::cout << board[i][j] << " ";
 		}
-		cout << "\n";
+		std::cout << "\n";
 	}
 
 	for (int i = 1; i <= n; i++)
 	{
 		for (int j = 1; j <= n; j++)
 		{
-			if (nxt[i][j] == 0)
+			if (board[i][j] == 0 || board[i][j] == INF)
 			{
-				cout << 0 << "\n";
+				std::cout << 0 << "\n";
 				continue;
 			}
-			vector<int> route;
-			int k = i;
-			route.push_back(i);
-			while (nxt[k][j] != j)
-			{
-				k = nxt[k][j];
-				route.push_back(k);
-			}
-			route.push_back(j);
 
-			cout << route.size() << " ";
-			for (const int& element : route)
-				cout << element << " ";
-			cout << "\n";
+			vector<int> pathes;
+			
+			int st = i;
+			pathes.push_back(st);
+			while (st != j)
+			{
+				st = nxt[st][j];
+				pathes.push_back(st);
+			}
+
+			std::cout << pathes.size() << " ";
+			for (auto e : pathes)
+				std::cout << e << " ";
+			std::cout << "\n";
+
 		}
 	}
 
