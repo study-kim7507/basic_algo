c66f7814efa9f09e31377ea398fa713bb70f6444
study-kim7507 <study.kim7507@gmail.com>
2025-11-21 15:28:38 +0900

    [Platinum V] Title: 오민식의 고민, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Platinum/1219. 오민식의 고민/오민식의 고민.cc
+++ 백준/Platinum/1219. 오민식의 고민/오민식의 고민.cc
@@ -1,17 +1,17 @@
 // BOJ 1219. 오민식의 고민
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <climits>
 #include <tuple>
+#include <queue>
 #include <algorithm>
 using namespace std;
 
-int st, en, n, m;
-vector<tuple<int, int, int>> edges;
-vector<int> money;
+#define INF 987654321
 
-long long dist[51];
+int n, st, en, m;
+vector<tuple<int, int, int>> edges;		// 교통수단, a에서 b로 가는데 드는 비용 c
+vector<int> canEarn;					// 현재 도시에 도착할 경우 얻을 수 있는 돈
+vector<long long> earned;
 
 bool bfs(queue<int>& q, vector<bool>& visit)
 {
@@ -20,66 +20,56 @@ bool bfs(queue<int>& q, vector<bool>& visit)
 		auto cur = q.front(); q.pop();
 		for (auto edge : edges)
 		{
-			int from, to, cost;
-			tie(from, to, cost) = edge;
+			int u, v, c;
+			tie(u, v, c) = edge;
 
-			if (from != cur) continue;
-			if (visit[to]) continue;
+			if (cur != u) continue;
+			if (visit[v]) continue;
 
-			visit[to] = true;
-			q.push(to);
+			visit[v] = true;
+			q.push(v);
 		}
 	}
 
 	return visit[en];
 }
 
-bool bellmanford()
+void bellmanford(bool& hasCycle, bool& canArrive)
 {
-	fill(dist, dist + 51, -LLONG_MAX);
-	dist[st] = money[st];
+	earned = vector<long long>(n + 1, -INF);
+	earned[st] = canEarn[st];
 
-	for (int i = 1; i <= n - 1; i++)
+	for (int i = 0; i < n - 1; i++)
 	{
 		for (int j = 0; j < edges.size(); j++)
 		{
 			int from, to, cost;
 			tie(from, to, cost) = edges[j];
 
-			if (dist[from] == -LLONG_MAX) continue;
-
-			if (dist[to] < dist[from] + cost)
-				dist[to] = dist[from] + cost;
+			if (earned[from] == -INF) continue;
+			if (earned[to] < earned[from] + cost + canEarn[to])
+				earned[to] = earned[from] + cost + canEarn[to];
 		}
 	}
 
 	queue<int> q;
-	vector<bool> visit(n, false);
-
-	for (int j = 0; j < edges.size(); j++)
+	vector<bool> visit(n + 1, false);
+	for (int i = 0; i < edges.size(); i++)
 	{
 		int from, to, cost;
-		tie(from, to, cost) = edges[j];
+		tie(from, to, cost) = edges[i];
 
-		if (dist[from] == -LLONG_MAX) continue;
-
-		if (dist[to] < dist[from] + cost)
+		if (earned[from] == -INF) continue;
+		if (earned[to] < earned[from] + cost + canEarn[to])
 		{
-			if (!visit[to])
-			{
-				visit[to] = true;
-				q.push(to);
-			}
+			hasCycle = true;
+			q.push(to);
+			visit[to] = true;
 		}
 	}
 
-	if (!q.empty())
-		return bfs(q, visit);
-	else
-		return false;
+	canArrive = bfs(q, visit);
 }
-
-
 
 int main()
 {
@@ -94,33 +84,22 @@ int main()
 		edges.push_back(make_tuple(u, v, -c));
 	}
 
-	money = vector<int>(n, 0);
 	for (int i = 0; i < n; i++)
-		cin >> money[i];
-
-	for (int i = 0; i < m; i++)
 	{
-		int u, v, c;
-		tie(u, v, c) = edges[i];
-		
-		c += money[v];
-		edges[i] = make_tuple(u, v, c);
+		int earn;
+		cin >> earn;
+		canEarn.push_back(earn);
 	}
 
-	// 양의 무한 사이클이 있는 경우
-	bool hasCycleToEn = bellmanford();
+	bool hasCycle = false;
+	bool canArrive = false;
+	bellmanford(hasCycle, canArrive);
 
-	if (hasCycleToEn)
-	{
-		std::cout << "Gee" << "\n";
-	}
+	if (hasCycle && canArrive) std::cout << "Gee" << "\n";
 	else
 	{
-		// st에서 en까지 경로가 존재한다면 (dist가 갱신되었다면) 최대 이익 출력
-		if (dist[en] != -LLONG_MAX) std::cout << dist[en] << "\n";
-		// 경로가 없다면
-		else std::cout << "gg" << "\n";
+		if (earned[en] == -INF) std::cout << "gg" << "\n";
+		else std::cout << earned[en] << "\n";
 	}
-	
 	return 0;
 }
